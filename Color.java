d46ce529c827bd05f0cced80a78adc024887441c
Your Name <you@example.com>
2018-03-18 21:11:25 -0400

    enum type made

--- Color.java
+++ Color.java
@@ -1,7 +1,9 @@
 public class Color{
 
+  public static final int BLUE = 1;
+  public static final int RED = 2;
+  public static final int GREEN = 3;
+  public static final int WHITE = 4;
 
 
-
-  
 }
