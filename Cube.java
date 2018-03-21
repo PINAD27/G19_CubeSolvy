cc271c225cae5b40e442c4bcdfb3867ce95089a5
Your Name <you@example.com>
2018-03-21 14:24:16 -0400

    added new storage class state for inital variable

--- Cube.java
+++ Cube.java
@@ -1,7 +1,21 @@
 public class Cube{
+
+	private static class State{
+		public Color[] in;
+		public State(Color[] in){
+			this.in = in;
+		}
+		public void print(){
+			for(int i = 0; i < 6; i++){
+				System.out.println(in[i]);
+			}
+		}
+	}
+
 	private Color[] faces = new Color[6];
-  public static final Queue<Cube> queue = new LinkedQueue<Cube>(); //MAKE PRIVATE LATER DELETEm
   private int rotations;
+	private State initial;
+	//private State[] states = new State[24]; //stores cube states
   private String[] order = (new String[]{"I","R","R","R","RR","R","R","R","RR","R","R","R","LR","R","R","R","LR","R","R","R","RR","R","R","R"});
 
 	public enum Color {
@@ -9,17 +23,23 @@ public class Cube{
  	}
 
 	public Cube(Color[] faces){
-
 		this.faces = faces;
-    Cube c = this;
-    queue.enqueue(c);
+    initial = new State(faces);
   }
+
+	public Cube(Cube other){ //copied cube
+		Cube c = other.copy();
+		this.faces = c.faces;
+		this.rotations = c.rotations;
+	}
+
 
   public boolean hasNext(){
     return rotations < 23;
   }
 
   public void next() throws IllegalStateException{
+
     if(order[rotations].equals("I")){
       Identity();
     }
@@ -35,60 +55,47 @@ public class Cube{
     else{
       throw new IllegalStateException();
     }
+		rotations++;
   }
-  /*
-  public void reset(){
 
-
-  }
-  */
+  	public void reset(){
+			Identity();
+  	}
 
   public void Rotate(){
-    Color rotated[] = faces;
-    Color front = rotated[1];
-    Color right = rotated[2];
-    Color back = rotated[3];
-    Color left = rotated[4];
+    Color front = faces[1];
+    Color right = faces[2];
+    Color back = faces[3];
+    Color left = faces[4];
     //front = left, right = front,  back = right, left = back
-    rotated[1] = left;
-    rotated[2] = front;
-    rotated[3] = right;
-    rotated[4] = back;
-    Cube c = new Cube(rotated);
-    queue.enqueue(c);
-    rotations++;
+    faces[1] = left;
+    faces[2] = front;
+    faces[3] = right;
+    faces[4] = back;
   }
 
   public void RightRoll(){
-    Color rotated[] = faces;
-    Color up = rotated[0];
-    Color right = rotated[2];
-    Color left = rotated[4];
-    Color down = rotated[5];
+    Color up = faces[0];
+    Color right = faces[2];
+    Color left = faces[4];
+    Color down = faces[5];
     //up = left, right = up, left = down, down = right
-    rotated[0] = left;
-    rotated[2] = up;
-    rotated[4] = down;
-    rotated[5] = right;
-    Cube c = new Cube(rotated);
-    queue.enqueue(c);
-    rotations++;
+    faces[0] = left;
+    faces[2] = up;
+    faces[4] = down;
+    faces[5] = right;
   }
 
   public void LeftRoll(){
-    Color rotated[] = faces;
-    Color up = rotated[0];
-    Color right = rotated[2];
-    Color left = rotated[4];
-    Color down = rotated[5];
+    Color up = faces[0];
+    Color right = faces[2];
+    Color left = faces[4];
+    Color down = faces[5];
     //up = right, right = down, left = up, down = left
-    rotated[0] = right;
-    rotated[2] = down;
-    rotated[4] = up;
-    rotated[5] = left;
-    Cube c = new Cube(rotated);
-    queue.enqueue(c);
-    rotations++;
+    faces[0] = right;
+    faces[2] = down;
+    faces[4] = up;
+    faces[5] = left;
   }
 
 
@@ -129,19 +136,46 @@ public class Cube{
 	}
 
   public void Identity(){
-    Cube c = queue.getFront();
-    System.out.println("identin " + c);
-    faces = c.faces;
+    faces = initial.in;
   }
+
+	public void setInitialState(State s){
+		this.initial = s;
+	}
+
+	public void setRotations(int r){
+		this.rotations = r;
+	}
+
+	
+	public Cube copy(){ //cube copy
+		Cube c;
+		int c_rotations = this.rotations;
+		Color[] c_faces = new Color[6];
+		State c_initial = null;
+		for(int i = 0; i < 6; i++){
+			c_faces[i] = faces[i];
+		}
+		for(int i = 0; i < rotations; i++){
+			c_initial.in[i] = initial.in[i];
+		}
+		c = new Cube(c_faces);
+		c.setInitialState(c_initial);
+		c.setRotations(c_rotations);
+		return c;
+	}
 
 public static void main(String[] args) {
 		Cube c;
 	 c = new Cube ( new Color [] { Color.BLUE, Color.GREEN, Color.WHITE, Color.GREEN, Color.BLUE, Color.RED}) ;
+
    int count = 0;
    while(count++ < 24){
      c.next();
      System.out.println(c);
    }
+	 c.Identity();
+	 System.out.println(c);
    /*
 	 System.out.println(c);
    c.Rotate();
