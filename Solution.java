686cd02fca119a582446391f38b002c85cc47ac2
Your Name <you@example.com>
2018-03-25 16:25:01 -0400

    isValid() done

--- Solution.java
+++ Solution.java
@@ -23,11 +23,37 @@ public class Solution{
   }
 
   public Cube getCube(int pos){
+    if (pos > size()){
+      throw new IndexOutOfBoundsException("position is out of bounds of the data struct");
+    }
     return cube[pos];
   }
 
   public boolean isValid(){
+    //front right left back
+    boolean sides = new boolean[size()]; //stores the validation of all cubes
+    for(int i = 0; i < size()-1; i++){
+      sides[i] = validate(i);
+    }
+    boolean valid = true;
+    for(int j = 0; j < sides.length; j++){
+      if(sides[j] == false){
+        valid = false;
+        break;
+      }
+    }
+    return valid;
+  }
 
+  public boolean validate(int pos){
+    Cube c = getCube(pos);
+    for(int i = 1; i < (size()-pos); i++){
+      boolean f = c.getFront() != cubes[i].getFront();
+      boolean r = c.getRight() != cubes[i].getRight();
+      boolean l = c.getLeft() != cubes[i].getLeft();
+      boolean b = c.getBack() != cubes[i].getBack();
+    }
+    return f == r == l == b;
   }
 
   public boolean isValid(Cube next){
